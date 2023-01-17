810a717b5b11d28e7bf08aa0c76d078f81516f50
mkmnska <117698135+mkmnska@users.noreply.github.com>
2023-01-17 16:31:55 +0100

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    class triangle:
+        def __init__(self, a, h):
+            self.a = a
+            self.h = h
+
+
+    triangle1 = triangle(3, 2)
+
+    print(triangle1.a * triangle1.h*0.5)
